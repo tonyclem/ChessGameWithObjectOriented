bfb0fb18e3ed7714c284f2103c46e4d6e64bcda6
tonyclem <tonyclem001@gmail.com>
2022-03-04 12:02:54 +0100

    Chess game with object oriented Programming

---
+++ services/piecesHandle/handlePieceEvents/handlePieceClick.js
@@ -0,0 +1,82 @@
+import { playerTurn } from "../../../services/playerTurn.service.js";
+import { checkMate } from "../../checkMate.service.js";
+import { piecesDetermine } from "../../piecesDetermine.service.js";
+import handlePieceMovingHelpers from "../helpers/handlePieceMoving.helpers.js";
+
+export default {
+  handlePieceClick({
+    pieceBoxElement,
+    pieceBoxPosition,
+    pieceElement,
+    pieceType,
+  }) {
+    if (checkMate.gameOver) {
+      return;
+    }
+
+    const { pieceSelectedPosition } = this;
+
+    if (
+      piecesDetermine.hasPiecePotential(pieceSelectedPosition, pieceBoxPosition)
+    ) {
+      if (
+        checkMate.cantMoveDueToCheck({
+          pieceSelectedPosition,
+          pieceBoxPosition,
+        })
+      ) {
+        this.setNotAllowed(pieceBoxElement);
+        return;
+      }
+
+      this.handleMovingThePiece({ pieceBoxElement, pieceElement });
+
+      if (checkMate.isCheckMate()) {
+        checkMate.displayCheckMateMessage();
+      }
+      return;
+    }
+
+    if (!pieceElement) {
+      return;
+    }
+    if (this.isPieceSelected() && this.isNotOnPieceSelected(pieceBoxPosition)) {
+      return;
+    }
+    // if the piece is selected don't left other piece click
+    if (this.isOnPieceSelected(pieceBoxPosition)) {
+      this.resetPieceSelected();
+      this.removeReady(pieceBoxElement);
+      this.removePiecePotentials(pieceBoxPosition);
+
+      return;
+    }
+
+    if (playerTurn.isRightTurn(pieceType)) {
+      if (this.hasPiecePotentials(pieceBoxPosition)) {
+        this.changePieceSelected(pieceBoxPosition);
+        this.setReady(pieceBoxElement);
+        this.setPiecePotentials(pieceBoxPosition);
+      } else {
+        this.setNotAllowed(pieceBoxElement);
+      }
+
+      return;
+    } else if (!this.isPieceSelected()) {
+      this.setNotAllowed(pieceBoxElement);
+      return;
+    }
+  },
+  // allow the piece to move dynamically
+  setPiecePotentials(pieceBoxPosition) {
+    piecesDetermine.itereateDetermine(pieceBoxPosition, this.setPotential);
+  },
+  removePiecePotentials(pieceBoxPosition) {
+    piecesDetermine.itereateDetermine(pieceBoxPosition, this.removePotential);
+  },
+  hasPiecePotentials(pieceBoxPosition) {
+    return piecesDetermine.hasPiecePotentials(pieceBoxPosition);
+  },
+
+  ...handlePieceMovingHelpers,
+};
